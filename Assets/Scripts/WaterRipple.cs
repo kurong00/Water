b5f80a6a008fbe67b0e39c0d09ee99104c7a20e2
kurong00 <kurong00@gmail.com>
2018-02-22 22:06:45 +0800

    edit WaterRipple

--- Assets/Scripts/WaterRipple.cs
+++ Assets/Scripts/WaterRipple.cs
@@ -6,20 +6,22 @@ using System.IO;
 using System.Threading;
 public class WaterRipple : MonoBehaviour {
 
-    Vector4 Amplitude;
-    Vector4 Frequency;
-    Vector4 Steepness;
-    Vector4 Speed;
-    Vector4 DirectionAB;
-    Vector4 DirectionCD;
-    Texture2D WaterDisplacementTexture;
-    Vector3[] WavePoints;
-    Color[] Colors;
-    Vector2[,] WaveAcceleration;
-    float[] CutOutTextureGray;
-    bool CutOutTextureInitialized;
-    DateTime UpdateDateTime;
-    double ThreadDeltaTime;
+    Vector4 amplitude;
+    Vector4 frequency;
+    Vector4 steepness;
+    Vector4 speed;
+    Vector4 directionAB;
+    Vector4 directionCD;
+    Texture2D waterDisplacementTexture;
+    Vector3[] wavePoints;
+    Color[] colors;
+    Vector2[,] waveAcceleration;
+    float[] cutOutTextureGray;
+    bool cutOutTextureInitialized;
+    DateTime updateDateTime;
+    double threadDeltaTime;
+    Vector2 objectPos, projectorPosition;
+    Vector3 scaleBounds;
 
     [Range(20, 200)]
     public int UpdateFPS = 60;
@@ -40,12 +42,13 @@ public class WaterRipple : MonoBehaviour {
     private void Awake()
     {
         var water = GetComponent<Renderer>();
-        Amplitude = water.sharedMaterial.GetVector("_Amplitude");
-        Frequency = water.sharedMaterial.GetVector("_Frequency");
-        Steepness = water.sharedMaterial.GetVector("_Steepness");
-        Speed = water.sharedMaterial.GetVector("_Speed");
-        DirectionAB = water.sharedMaterial.GetVector("_DirectionAB");
-        DirectionCD = water.sharedMaterial.GetVector("_DirectionCD");
+        amplitude = water.sharedMaterial.GetVector("_Amplitude");
+        frequency = water.sharedMaterial.GetVector("_Frequency");
+        steepness = water.sharedMaterial.GetVector("_Steepness");
+        speed = water.sharedMaterial.GetVector("_Speed");
+        directionAB = water.sharedMaterial.GetVector("_DirectionAB");
+        directionCD = water.sharedMaterial.GetVector("_DirectionCD");
+        scaleBounds = GetComponent<MeshRenderer>().bounds.size;
         InitRipple();
     }
     void Start () {
@@ -59,43 +62,73 @@ public class WaterRipple : MonoBehaviour {
 
     void InitRipple()
     {
-        WaterDisplacementTexture = new Texture2D(ResolutionTexture, ResolutionTexture, TextureFormat.RGBA32, false);
-        WaterDisplacementTexture.wrapMode = TextureWrapMode.Clamp;
-        WaterDisplacementTexture.filterMode = FilterMode.Bilinear;
-        Shader.SetGlobalTexture("_WaterDisplacementTexture", WaterDisplacementTexture);
-        WavePoints = new Vector3[ResolutionTexture * ResolutionTexture];
-        Colors = new Color[ResolutionTexture * ResolutionTexture];
-        WaveAcceleration = new Vector2[ResolutionTexture, ResolutionTexture];
+        waterDisplacementTexture = new Texture2D(ResolutionTexture, ResolutionTexture, TextureFormat.RGBA32, false);
+        waterDisplacementTexture.wrapMode = TextureWrapMode.Clamp;
+        waterDisplacementTexture.filterMode = FilterMode.Bilinear;
+        Shader.SetGlobalTexture("_WaterDisplacementTexture", waterDisplacementTexture);
+        wavePoints = new Vector3[ResolutionTexture * ResolutionTexture];
+        colors = new Color[ResolutionTexture * ResolutionTexture];
+        waveAcceleration = new Vector2[ResolutionTexture, ResolutionTexture];
         for(int i = 0; i < ResolutionTexture * ResolutionTexture; i++)
         {
-            Colors[i] = Color.black;
-            WavePoints[i] = Vector3.zero;
+            colors[i] = Color.black;
+            wavePoints[i] = Vector3.zero;
         }
         for (int i = 0; i < ResolutionTexture; i++)
             for (int j = 0; j < ResolutionTexture; j++)
-                WaveAcceleration[i, j] = Vector2.zero;
+                waveAcceleration[i, j] = Vector2.zero;
         if(!CutOutTexture)
         {
             //var scaleOfCutOutTexture = ScaleTexture(CutOutTexture, ResolutionTexture, ResolutionTexture);
             var scaleOfCutOutTexture = CutOutTexture;
             var colors = scaleOfCutOutTexture.GetPixels();
-            CutOutTextureGray = new float[ResolutionTexture * ResolutionTexture];
+            cutOutTextureGray = new float[ResolutionTexture * ResolutionTexture];
             for (int i = 0; i < colors.Length; i++)
                 //直接采用心理学灰度值公式
                 //https://en.wikipedia.org/wiki/Grayscale
-                CutOutTextureGray[i] = colors[i].r * 0.299f + colors[i].g * 0.587f + colors[i].b * 0.114f;
-            CutOutTextureInitialized = true;
+                cutOutTextureGray[i] = colors[i].r * 0.299f + colors[i].g * 0.587f + colors[i].b * 0.114f;
+            cutOutTextureInitialized = true;
         }
     }
 
     void UpdateRipple()
     {
-        UpdateDateTime = DateTime.UtcNow;
-        ThreadDeltaTime = (DateTime.UtcNow - UpdateDateTime).TotalMilliseconds / 1000;
-        var sleepTime = (int)(1000f / UpdateFPS - ThreadDeltaTime);
+        updateDateTime = DateTime.UtcNow;
+        threadDeltaTime = (DateTime.UtcNow - updateDateTime).TotalMilliseconds / 1000;
+        var sleepTime = (int)(1000f / UpdateFPS - threadDeltaTime);
         if (sleepTime > 0)
             Thread.Sleep(sleepTime);
         //TODO
+    }
+    void CalculateRippleTexture()
+    {
+        if (UseProjectedWaves)
+            UpdateProjector();
+        var length = wavePoints.Length;
+
+    }
+
+    void UpdateProjector()
+    {
+        var xOffset = (int)(ResolutionTexture * objectPos.x / scaleBounds.x - projectorPosition.x);
+        var yOffset = (int)(ResolutionTexture * objectPos.y / scaleBounds.y - projectorPosition.y);
+        projectorPosition.x += xOffset;
+        projectorPosition.y += yOffset;
+        if (xOffset == 0 && yOffset == 0)
+            return;
+        if (xOffset >= 0 && yOffset >= 0)
+            for (int i = 1; i < ResolutionTexture; i++)
+            {
+                for (int j = 0; j < ResolutionTexture; j++)
+                {
+                    if (i + yOffset > 0 && i + yOffset < ResolutionTexture && j + xOffset > 0 && j + xOffset < ResolutionTexture)
+                    {
+                        waveAcceleration[j, i] = waveAcceleration[j + xOffset, i + yOffset];
+                        wavePoints[j + (i * ResolutionTexture)] = wavePoints[j + xOffset + ((i + yOffset) * ResolutionTexture)];
+                    }
+                }
+            }
+
     }
 
     private Texture2D ScaleTexture(Texture2D source, int width, int height)
