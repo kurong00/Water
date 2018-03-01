1517446dcf78810f8da88ac76fa08e2a8fa91bcb
kurong00 <kurong00@gmail.com>
2018-03-01 17:00:06 +0800

    add CameraFollow.cs

---
+++ Assets/Scripts/Help/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public int speed;
+    Vector3 offset;
+	void Start () {
+        offset = transform.position - target.transform.position;
+	}
+	
+
+	void FixedUpdate () {
+        Vector3 cameraPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, cameraPos, Time.deltaTime * speed);
+
+	}
+}
