7063e11bdb0a433b38f2e88ea1a1dfd3209ba1f0
kurong00 <kurong00@gmail.com>
2018-03-04 11:55:03 +0800

    add JoyStick.cs

---
+++ Assets/Scripts/Help/JoyStick.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoyStick : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+
+    public static JoyStick instance = null;
+
+    public float dragSpeed = 6f;
+
+    private float screen;
+
+    private Vector2 centerPos;
+
+    private float horizon;
+    private float vertical;
+    public Vector2 CenterPos
+    {
+        set { centerPos = value; }
+    }
+
+    public float Horizon
+    {
+        get { return horizon; }
+    }
+    public float Vertical
+    {
+        get { return vertical; }
+    }
+
+    void Awake()
+    {
+
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+
+    }
+    void Start()
+    {
+        screen = 1f * Screen.width / 40; 
+        centerPos = GetComponent<RectTransform>().position;
+    }
+    void GetAxis(Vector2 pos)
+    { 
+        Vector2 diff = pos - centerPos;
+        float distance = diff.magnitude;
+        if (distance > screen)
+        {
+            pos = centerPos + diff / distance * screen;
+
+        }
+        GetComponent<RectTransform>().position = pos;
+        Vector2 move = pos - centerPos;
+        horizon = move.x;
+        vertical = move.y;
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        Vector2 newPos = new Vector2(data.position.x - 20f, data.position.y - 20f);
+        GetAxis(newPos);
+    }
+
+    public void OnEndDrag(PointerEventData data)
+    {
+        GetComponent<RectTransform>().position = centerPos;
+        GetAxis(centerPos);
+    }
+
+}
--- Assets/Scripts/Help/ObjectMove.cs
+++ Assets/Scripts/Help/ObjectMove.cs
@@ -4,53 +4,35 @@ using UnityEngine;
 
 public class ObjectMove : MonoBehaviour {
 
-    public int speedMultiple;
-    Rigidbody rigid;
-    bool goUp, goLeft, goBack, goRight;
-    void Start()
-    {
-        rigid = GetComponent<Rigidbody>();
-    }
-    
+    public float moveSpeed = 5;
+    public float rotateSpeed = 15;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-            goUp = true;
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-            goLeft = true;
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-            goBack = true;
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-            goRight = true;
+        Move();
+    }
 
-        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow))
-            goUp = false;
-        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
-            goLeft = false;
-        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
-            goBack = false;
-        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
-            goRight = false;
-
-        if (goUp)
+    void Move()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || JoyStick.instance.Horizon != 0)
         {
-            transform.Translate(-transform.forward * 5 * speedMultiple * Time.deltaTime);
-            //rigid.AddForce(transform.right * 5 * speedMultiple * Time.deltaTime);
+            if (Input.GetAxis("Horizontal") != 0)
+            {
+                transform.Rotate(Vector3.up * rotateSpeed * Input.GetAxis("Horizontal") * 2 * Time.deltaTime);
+            }
+            else
+                transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime *
+                JoyStick.instance.Horizon / JoyStick.instance.dragSpeed);
         }
-        if (goBack)
+        if (Input.GetAxis("Vertical") != 0 || JoyStick.instance.Vertical != 0)
         {
-            transform.Translate(transform.forward * 5 * speedMultiple * Time.deltaTime);
-            //rigid.AddForce(transform.right * 5 * speedMultiple * Time.deltaTime);
-        }
-        if (goRight)
-        {
-            transform.Translate(-transform.right * 5 * speedMultiple * Time.deltaTime);
-            //rigid.AddTorque(transform.forward * 2 * speedMultiple * Time.deltaTime);
-        }
-        if (goLeft)
-        {
-            transform.Translate(transform.right * 5 * speedMultiple * Time.deltaTime);
-            //rigid.AddTorque(-transform.forward * 2 * speedMultiple * Time.deltaTime);
+            if (Input.GetAxis("Vertical") != 0)
+            {
+                transform.Translate(Vector3.right * Input.GetAxis("Vertical") * moveSpeed * 2 * Time.deltaTime);
+            }
+            else
+                transform.Translate(Vector3.right * moveSpeed * Time.deltaTime *
+              JoyStick.instance.Vertical / JoyStick.instance.dragSpeed);
         }
     }
 }
