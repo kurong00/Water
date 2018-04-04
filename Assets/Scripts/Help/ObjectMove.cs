6d601b7c76ff7c7a8c8d975f505d5f821939f51d
kurong00 <kurong00@gmail.com>
2018-04-04 21:28:18 +0800

    update prefab

--- Assets/Scripts/Help/ObjectMove.cs
+++ Assets/Scripts/Help/ObjectMove.cs
@@ -14,24 +14,24 @@ public class ObjectMove : MonoBehaviour {
 
     void Move()
     {
-        if (Input.GetAxis("Horizontal") != 0 || JoyStick.instance.Horizon != 0)
+        if (Input.GetAxis("Horizontal") != 0)
         {
             if (Input.GetAxis("Horizontal") != 0)
             {
                 transform.Rotate(Vector3.down * rotateSpeed * Input.GetAxis("Horizontal") * 2 * Time.deltaTime);
             }
-            else
-                transform.Rotate(Vector3.down * rotateSpeed * Time.deltaTime/2 *
+            else if (JoyStick.instance != null && JoyStick.instance.Horizon != 0)
+                transform.Rotate(Vector3.down * rotateSpeed * Time.deltaTime / 2 *
                 JoyStick.instance.Horizon / JoyStick.instance.dragSpeed);
         }
-        if (Input.GetAxis("Vertical") != 0 || JoyStick.instance.Vertical != 0)
+        if (Input.GetAxis("Vertical") != 0)
         {
             if (Input.GetAxis("Vertical") != 0)
             {
                 transform.Translate(Vector3.right * Input.GetAxis("Vertical") * moveSpeed * 2 * Time.deltaTime);
             }
-            else
-                transform.Translate(Vector3.right * moveSpeed * Time.deltaTime/2 *
+            else if (JoyStick.instance != null && JoyStick.instance.Vertical != 0)
+                transform.Translate(Vector3.right * moveSpeed * Time.deltaTime / 2 *
               JoyStick.instance.Vertical / JoyStick.instance.dragSpeed);
         }
     }
