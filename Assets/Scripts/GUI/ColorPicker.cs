c7386d1c7bb03d5bb51db8bf8fce5f89e64779e2
kurong00 <kurong00@gmail.com>
2018-04-14 21:47:40 +0800

    edit Sea.unity

--- Assets/Scripts/GUI/ColorPicker.cs
+++ Assets/Scripts/GUI/ColorPicker.cs
@@ -35,7 +35,6 @@ public class ColorPicker : MonoBehaviour {
 	float alphaGradientHeight = 16;
 
 	GUIStyle titleStyle = null;
-    GUIStyle textureStyle = null;
     Color textColor = Color.white;
 	Texture2D txColorDisplay;
 
